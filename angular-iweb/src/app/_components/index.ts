39dd9e6992a883b3fc5c6cf99050b9ae8c9d59ad
Taros97 <nestor_anton@hotmail.es>
2019-12-31 13:02:02 +0100

    Añadido About

--- angular-iweb/src/app/_components/index.ts
+++ angular-iweb/src/app/_components/index.ts
@@ -2,3 +2,4 @@ export * from './alert/alert.component';
 export * from './home/home.component';
 export * from './login/login.component';
 export * from './register/register.component';
+export * from './about/about.component';
