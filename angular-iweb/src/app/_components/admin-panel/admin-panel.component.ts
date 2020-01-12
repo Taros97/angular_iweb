63849a0da32235fb6c61fb08ab1f57a1c4ead9be
Taros97 <nestor_anton@hotmail.es>
2020-01-12 18:34:33 +0100

    Añadido panel de control basico

--- angular-iweb/src/app/_components/admin-panel/admin-panel.component.ts
+++ angular-iweb/src/app/_components/admin-panel/admin-panel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+
 
 @Component({
   selector: 'app-admin-panel',
@@ -7,9 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPanelComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
+  ngOnInit() {}
+  shouldRun = true;
 
 }
